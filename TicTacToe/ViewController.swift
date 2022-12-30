a36396147884b52e54dd116b43e356fe497e4c46
mahmadali3180 <mahmadali3180@gmail.com>
2022-12-30 16:33:45 +0500

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by M. Ahmad Ali on 30/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
